0bf266b9d10f3de1f9d18c2b6ce49bd2642217f4
Dominic Cleal <dominic.cleal@freeagent.com>
2020-05-22 15:52:50 +0100

    Migrate schedule check to new logging output

--- lib/fa-harness-tools/check_schedule.rb
+++ lib/fa-harness-tools/check_schedule.rb
@@ -9,22 +9,33 @@ module FaHarnessTools
   class CheckSchedule
     def initialize(timezone: "Europe/London")
       tz = TZInfo::Timezone.get(timezone)
+      @timezone = timezone
       @now = tz.to_local(Time.now.utc)
+      @logger = CheckLogger.new(
+        name: "Check deployment schedule",
+        description: "Only allow deployments within certain times of the day",
+      )
     end
 
     def verify?
+      @logger.start
+      @logger.info("operating in the #{@timezone} timezone")
+      @logger.info("local time is #{@now}")
+
       permitted = false
       case @now.wday
       when 1..4
+        @logger.info("deployments are allowed between 9am to 4pm today (Mon-Thu)")
         permitted = true if @now.hour >= 9 && @now.hour < 16
       when 5
+        @logger.info("deployments are allowed between 9am to 12pm today (Fri)")
         permitted = true if @now.hour >= 9 && @now.hour < 12
       end
 
       if permitted
-        [true, "scheduled deploy time"]
+        @logger.pass "inside the deployment schedule"
       else
-        [false, "outside deployment schedule"]
+        @logger.fail "outside the deployment schedule"
       end
     end
   end
